b1b42f2dc76a369ce6bbb667d8654044f57e8533
hbanhx <51317341+hbanhx@users.noreply.github.com>
2026-05-30 22:11:27 +0200

    .

--- transform/transform.py
+++ transform/transform.py
@@ -20,18 +20,18 @@ def prepare_amazon_order(am_order_data):
         - data["ship-promotion-discount"]
         ) 
     
-    data["vat_pct"] = round(data["item-tax"] / data["unit_price"].where(data["quantity"] != 0), 2)
+    data["vat_pct"] = round(data["item-tax"] / data["unit_price"].where(data["quantity"] != 0), 2) #.astype(float)
 
     data["Sell-To Customer No."] = None
     data.loc[(data["vat_pct"].isna() | data["vat_pct"] == 0) & data["buyer-tax-registration-id"].notna(), "Sell-To Customer No."] = "CUS1025197"
-    data.loc[data["vat_pct"] == 19, "Sell-To Customer No."] = "CUS1002096"
+    data.loc[data["vat_pct"] == 0.19, "Sell-To Customer No."] = "CUS1002096"
 
 
     data["is_valid_amazon_de_order"] = (
         (data["is-amazon-invoiced"] == True)
         & (data["order-status"] == "Shipped")
         & (data["sales-channel"] == "Amazon.de")
-        & (data["vat_pct"].isin([0, pd.NA]))
+        & (data["vat_pct"].isin([0, 0.19]))
         & (data["currency"] == "EUR")
         & (~data["buyer-tax-registration-id"].str.contains(",", na=False))
         & (data["Sell-To Customer No."].notna())
