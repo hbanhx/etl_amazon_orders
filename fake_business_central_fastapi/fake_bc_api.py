85623925b397e5f970cd31809bc01d21c97a2aa6
hbanhx <51317341+hbanhx@users.noreply.github.com>
2026-06-09 17:07:23 +0200

    .

--- fake_business_central_fastapi/fake_bc_api.py
+++ fake_business_central_fastapi/fake_bc_api.py
@@ -46,4 +46,16 @@ def create_line(company_id: str, order_id: str, body: dict):
 
 @app.get("/fake_bc/companies/{company_id}/salesOrders")
 def get_orders(company_id: str):
-    return [order for order in SALESORDERS.values() if order["company_id"] == company_id]
+    orders = []
+    for order in SALESORDERS.values():
+        if order["company_id"] == company_id:
+            orders.append(order)
+    return orders
+
+@app.get("/fake_bc/companies/{company_id}/salesOrders/{order_id}")
+def get_order(company_id: str, order_id: str):
+    for order in SALESORDERS.values():
+        if order["company_id"] == company_id and order["id"] == order_id:
+            return order
+# fake_company
+# f290ca5b-b424-4e41-a743-9d44015b5852
